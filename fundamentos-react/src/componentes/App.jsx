8c5fbc2543e271b8e589d6a7cb1efd0190d391ff
michelearias <66805958+michelearias@users.noreply.github.com>
2022-01-28 09:28:51 -0300

    inclusão de novo exercício

--- fundamentos-react/src/componentes/App.jsx
+++ fundamentos-react/src/componentes/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import Primeiro from './componentes/basicos/Primeiro'
+import Aleatorio from "./componentes/basicos/Aleatorio";
+import Primeiro from "./componentes/basicos/Primeiro";
+import ComParametro from "./componentes/basicos/ComParametro";
+import Fragmento from "./componentes/basicos/Fragmento";
 
-import ComParametro from './componentes/basicos/ComParametro'
-
-import Fragmento from './componentes/basicos/Fragmento'
-
-export default function App(props){
-    return(
-    <div><strong>Olá React!</strong>
-    <Fragmento></Fragmento>
-    <ComParametro 
-    titulo="Segundo Componente" 
-    subtitulo="Muito legal"
-    aluno="Pedro" nota={9.3}></ComParametro>
+// eslint-disable-next-line import/no-anonymous-default-export
+export default _ => (
+    <div id="app">
+        <h1>Fundamentos de React</h1>
+        <Aleatorio min={1} max={60}></Aleatorio>
+        <Fragmento />
+        <ComParametro 
+            titulo="Situação do aluno" 
+            aluno="Pedro Silva"
+            nota={9.3}/>
+        <Primeiro></Primeiro>
     
     </div>
-    )
-}
+    
+)
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,29 +2,9 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-import Primeiro from './componentes/basicos/Primeiro'
-
-import ComParametro from './componentes/basicos/ComParametro'
-
-import Fragmento from './componentes/basicos/Fragmento'
-
-
-const tag = <strong>Olá React</strong>
+import App from './App'
 
 ReactDOM.render(
-    <div><strong>Olá React!</strong>
-    {tag}
-    <Primeiro></Primeiro>
-    <ComParametro 
-    titulo="Segundo Componente" 
-    subtitulo="Muito legal"
-    aluno="Pedro" nota={9.3}></ComParametro>
-     <ComParametro 
-    titulo="Terceiro Componente" 
-    subtitulo="Muito legal"
-    aluno="Pedro" nota={9.9}></ComParametro>
-    <Fragmento></Fragmento>
-    
-    </div>,
+   <App></App>,
     document.getElementById('root')
 )
