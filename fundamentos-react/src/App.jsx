92f9a045092b45685544b03e89121ca3ed5ede6c
michelearias <66805958+michelearias@users.noreply.github.com>
2022-01-31 11:55:38 -0300

    inclusão de novos exercícios

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import './App.css'
 
-
+import Familia from "./componentes/basicos/Familia";
+import Card from './componentes/layout/Card'
 import Aleatorio from "./componentes/basicos/Aleatorio";
 import Primeiro from "./componentes/basicos/Primeiro";
 import ComParametro from "./componentes/basicos/ComParametro";
@@ -8,16 +10,31 @@ import Fragmento from "./componentes/basicos/Fragmento";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default _ => (
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos de React</h1>
-        <Aleatorio min={1} max={60}></Aleatorio>
-        <Fragmento />
-        <ComParametro 
-            titulo="Situação do aluno" 
-            aluno="Pedro Silva"
-            nota={9.3}/>
-        <Primeiro></Primeiro>
-    
+
+        <div className="Cards">
+            <Card titulo=" #05 - Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Ferreira"></Familia>
+            </Card>
+            <Card titulo=" #04 - Desafio Aleatório" color="#FA6900">
+                <Aleatorio min={1} max={60}></Aleatorio>
+            </Card>
+            <Card titulo=" #03 - Fragmento" color="#E94C6F">
+                <Fragmento />
+            </Card>
+            <Card titulo=" #02 - Com Parâmetro" color="#E8B71A">
+                <ComParametro
+                titulo="Situação do aluno"
+                aluno="Pedro Silva"
+                nota={9.3}/>
+            </Card>
+            <Card titulo=" #01 - Primeiro Componente" color="#588C73">
+                <Primeiro></Primeiro>
+            </Card>
+            
+        </div>
+
     </div>
     
 )
--- fundamentos-react/src/componentes/layout/Card.jsx
+++ fundamentos-react/src/componentes/layout/Card.jsx
@@ -1,5 +1,27 @@
+import './Card.css'
 import React from 'react'
 
-export default props => {
-    return <div>Card</div>
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => {
+
+    const cardStyle = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00',
+
+    }
+
+    return (
+        <div className='Card' style={cardStyle
+            //{backgroundColor: props.color || '#008',
+            //borderColor: props.color || '#008'}
+        }>
+            <div className='Title'>{props.titulo}</div>
+            <div className='Content'>
+                {props.children}
+            </div>  
+        </div>
+
+    )
+    
+    
 }
